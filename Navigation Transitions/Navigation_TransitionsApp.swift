5ea2575f68985b181ef3e1f5c6b2413ab6079187
Karan Pal <palkaran@outlook.com>
2025-07-13 18:56:31 +0530

    Initial Commit

---
+++ Navigation Transitions/Navigation_TransitionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Navigation_TransitionsApp.swift
+//  Navigation Transitions
+//
+//  Created by Karan Pal on 13/07/25.
+//
+
+import SwiftUI
+
+@main
+struct Navigation_TransitionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
